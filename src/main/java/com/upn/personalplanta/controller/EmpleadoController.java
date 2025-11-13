4bf318052b91ddc7ef1d6e46683792ca958f72f2
Oscar Roman Quispe <oscarroman.pe@gmail.com>
2025-11-13 12:08:37 -0500

    More of EmpleadoController.java

--- src/main/java/com/upn/personalplanta/controller/EmpleadoController.java
+++ src/main/java/com/upn/personalplanta/controller/EmpleadoController.java
@@ -1,10 +1,11 @@
 package com.upn.personalplanta.controller;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import com.upn.personalplanta.model.Empleado;
 import com.upn.personalplanta.service.EmpleadoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -33,4 +34,16 @@ public class EmpleadoController {
         return "redirect:/empleados";
     }
 
+    @GetMapping("/actualizarEmpleado/{id}")
+    public String actualizarEmpleado(@PathVariable(value="id") Integer id, Model model) {
+        Empleado empleado = empleadoService.obtenerEmpleadoPorId(id) ;
+        model.addAttribute("empleado", empleado);
+        return "/empleados/actualizarEmpleado";
+    }
+
+    @GetMapping("/eliminarEmpleado/{id}")
+    public String eliminarEmpleado(@PathVariable(value="id") Integer id) {
+        empleadoService.eliminarEmpleadoPorId(id);
+        return "redirect:/empleados";
+    }
 }
