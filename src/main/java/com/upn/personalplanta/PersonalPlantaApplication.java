2581794a117834e8907bff24bb669d7cefd8fd31
Oscar Roman Quispe <oscarroman.pe@gmail.com>
2025-11-12 22:35:46 -0500

    Initial setup

---
+++ src/main/java/com/upn/personalplanta/PersonalPlantaApplication.java
@@ -0,0 +1,13 @@
+package com.upn.personalplanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPlantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPlantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upn/personalplanta/PersonalPlantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upn.personalplanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPlantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
